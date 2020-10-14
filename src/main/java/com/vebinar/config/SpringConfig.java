efda1d8ec211149549fc86aee0f2931319e94569
dchalbyshev <dchalbyshev@gmail.com>
2020-10-15 01:01:42 +0300

    The pop.xml ok

---
+++ src/main/java/com/vebinar/config/SpringConfig.java
@@ -0,0 +1,15 @@
+package com.vebinar.config;
+
+
+import com.vebinar.service.TestBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfig {
+    @Bean
+    public TestBean getTestBean(){
+        return  new TestBean("Hello");
+    }
+
+}
---
+++ src/test/java/com/vebinar/config/SpringConfigTest.java
@@ -0,0 +1,9 @@
+package com.vebinar.config;
+
+import junit.framework.TestCase;
+
+public class SpringConfigTest extends TestCase {
+
+    public void testGetTestBean() {
+    }
+}
